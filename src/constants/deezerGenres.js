3c715e68101a73e485037d28db76ab53bc4f594a
OREL <aurelien.laridan62@gmail.com>
2026-05-02 09:59:33 +0200

    1.1 - adding deezer genres

---
+++ src/constants/deezerGenres.js
@@ -0,0 +1,29 @@
+// src/constants/deezerGenres.js
+// Mapping de NOS noms de genres vers les IDs Deezer
+// Source: https://api.deezer.com/genre
+
+export const DEEZER_GENRES = {
+  "Pop":         132,
+  "Hip-Hop":     116,
+  "Rock":        152,
+  "R&B":         165,
+  "Électro":     106,
+  "Jazz":        129,
+  "Classique":    98,
+  "Reggaeton":   464,
+  "K-Pop":      2914,
+  "Metal":       464,
+  "Soul":        169,
+  "Variété FR":   52,
+};
+
+// Décennies → plages d'années
+export const DECADE_RANGES = {
+  "60s":   [1960, 1969],
+  "70s":   [1970, 1979],
+  "80s":   [1980, 1989],
+  "90s":   [1990, 1999],
+  "2000s": [2000, 2009],
+  "2010s": [2010, 2019],
+  "2020s": [2020, 2029],
+};
